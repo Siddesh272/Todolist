57e1394309a6fbb2261a0f32466beecc422f2ecd
Siddesh272 <90123105+Siddesh272@users.noreply.github.com>
2023-07-22 21:34:32 +0530

    Final

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended :true}));
 app.use(express.static("public"));
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-siddesh:test-123@cluster0.sucerqf.mongodb.net/todolistDB",{useNewUrlParser: true});
 
 
 const itemsSchema={
